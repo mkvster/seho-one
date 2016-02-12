1ccc4c07f4047a43cb81cf3bb011afcd66df322b
mkvster <mkvster@gmail.com>
2016-02-11 20:12:56 -0500

    Added Self Hosting

---
+++ SehoOne/SehoOne/Startup.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using System.Configuration;
+using Microsoft.Owin.FileSystems;
+using Microsoft.Owin.StaticFiles;
+
+namespace SehoOne
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var siteRoot = Settings.Instance.SiteRoot;
+            var physicalFileSystem = new PhysicalFileSystem(siteRoot); 
+            var options = new FileServerOptions
+            {
+                EnableDefaultFiles = true,
+                FileSystem = physicalFileSystem
+            };
+            options.StaticFileOptions.FileSystem = physicalFileSystem;
+            options.StaticFileOptions.ServeUnknownFileTypes = true;
+            options.DefaultFilesOptions.DefaultFileNames = new[]
+            {
+                "index.html"
+            };
+
+            app.UseFileServer(options);
+        }
+    }
+}
